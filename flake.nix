523ff93e0e5a08d25a521ed57e5baa3e2e8c5377
Brian Gene Zhou <bz_ez@hotmail.com>
2025-01-03 20:55:21 -0500

    Add devshell flake.nix for ease of development

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        with pkgs;
+        {
+          devShells.default = mkShell {
+              nativeBuildInputs = [
+                rustc
+                cargo
+                clippy
+                cargo-edit
+                # https://github.com/Riey/cargo-feature/issues/35
+                #cargo-feature
+                rust-analyzer
+                rustfmt
+              ];
+
+              RUST_SRC_PATH = "${rust.packages.stable.rustPlatform.rustLibSrc}";
+
+              shellHook = ''
+                exec fish
+              '';
+          };
+        }
+      );
+}
